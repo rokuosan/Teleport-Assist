b8b507d09d0f7179a115f825deaa47f76c1cec7a
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-08-20 14:28:32 +0900

    GUIのイベントを登録した

--- src/main/kotlin/com/deviseworks/tpa/App.kt
+++ src/main/kotlin/com/deviseworks/tpa/App.kt
@@ -1,8 +1,11 @@
 package com.deviseworks.tpa
 
 import com.deviseworks.tpa.command.AcceptCommand
+import com.deviseworks.tpa.command.GuiCommand
 import com.deviseworks.tpa.command.RequestCommand
 import com.deviseworks.tpa.entity.PlayerStatus
+import com.deviseworks.tpa.inventory.ExampleGui
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 var requests = mutableListOf<PlayerStatus>()
@@ -15,6 +18,10 @@ class App: JavaPlugin(){
         getCommand("tpr")?.setExecutor(RequestCommand(this))
         getCommand("tpa")?.setExecutor(AcceptCommand())
 //        getCommand("sr")?.setExecutor(ShowList())
+        getCommand("g")?.setExecutor(GuiCommand())
+
+        logger.info("Registering events...")
+        Bukkit.getServer().pluginManager.registerEvents(ExampleGui(), this)
 
         logger.info("TPA is ready!")
     }
