fea553c4efe92441771d46f660707e92629500de
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-08-21 00:34:20 +0900

    Store.kt を追加

--- src/main/kotlin/com/deviseworks/tpa/App.kt
+++ src/main/kotlin/com/deviseworks/tpa/App.kt
@@ -1,11 +1,8 @@
 package com.deviseworks.tpa
 
 import com.deviseworks.tpa.command.AcceptCommand
-import com.deviseworks.tpa.command.GuiCommand
 import com.deviseworks.tpa.command.RequestCommand
 import com.deviseworks.tpa.entity.PlayerStatus
-import com.deviseworks.tpa.inventory.ExampleGui
-import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 var requests = mutableListOf<PlayerStatus>()
@@ -13,15 +10,13 @@ var requests = mutableListOf<PlayerStatus>()
 class App: JavaPlugin(){
 
     override fun onEnable() {
+        Store.plugin = this
 
         logger.info("Registering commands...")
         getCommand("tpr")?.setExecutor(RequestCommand(this))
         getCommand("tpa")?.setExecutor(AcceptCommand())
-//        getCommand("sr")?.setExecutor(ShowList())
-        getCommand("g")?.setExecutor(GuiCommand())
 
         logger.info("Registering events...")
-        Bukkit.getServer().pluginManager.registerEvents(ExampleGui(), this)
 
         logger.info("TPA is ready!")
     }
