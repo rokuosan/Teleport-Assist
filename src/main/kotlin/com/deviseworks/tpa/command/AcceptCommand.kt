d6159bd2b1b9da3f218579c2883c6cf6d0d9469d
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-08-21 01:21:46 +0900

    認証部を別メソッドに書き換えた

--- src/main/kotlin/com/deviseworks/tpa/command/AcceptCommand.kt
+++ src/main/kotlin/com/deviseworks/tpa/command/AcceptCommand.kt
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class AcceptCommand: CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -30,37 +31,8 @@ class AcceptCommand: CommandExecutor {
                     return true
                 }
 
-
-
-                // ターゲットが自分に対してリクエストを送信しているか
-                var flag = false
-                lateinit var status: PlayerStatus
-                var accepted: Boolean    // 承認済み
-                var existTarget: Boolean // 相手からのリクエストがあるか
-                var existSelf: Boolean   // 自分がターゲットになっているか
-                var expired: Boolean // 期限切れか
-                for(r in requests){
-                    status = r
-                    accepted = r.accepted
-                    existSelf = r.targetUUID == executor.uniqueId
-                    existTarget = r.uuid == target.uniqueId
-                    expired = r.expired
-
-                    if(!accepted && existSelf && existTarget && !expired){
-                        flag = true
-                        break
-                    }
-                }
-
-                if(flag){
-                    target.teleport(executor.location)
-                    sender.sendMessage(Messages.PERMIT_TELEPORT)
-                    target.sendMessage(Messages.SUCCESSFULLY_TELEPORT)
-                    status.accepted = true
-                }else{
-                    sender.sendMessage(Messages.NOT_FOUND_REQUEST)
-                    sender.sendMessage(Messages.HINT_RESEND_REQUEST)
-                }
+                // 受理
+                acceptRequest(executor, target)
 
             }else{
                 sender.sendMessage(Messages.REQUIRE_TARGET_PLAYER)
@@ -69,6 +41,38 @@ class AcceptCommand: CommandExecutor {
             return true
         }else{
             return false
+        }
+    }
+
+    private fun acceptRequest(executor: Player, target: Player){
+        // ターゲットが自分に対してリクエストを送信しているか
+        var flag = false
+        lateinit var status: PlayerStatus
+        var accepted: Boolean    // 承認済み
+        var existTarget: Boolean // 相手からのリクエストがあるか
+        var existSelf: Boolean   // 自分がターゲットになっているか
+        var expired: Boolean // 期限切れか
+        for(r in requests){
+            status = r
+            accepted = r.accepted
+            existSelf = r.targetUUID == executor.uniqueId
+            existTarget = r.uuid == target.uniqueId
+            expired = r.expired
+
+            if(!accepted && existSelf && existTarget && !expired){
+                flag = true
+                break
+            }
+        }
+
+        if(flag){
+            target.teleport(executor.location)
+            executor.sendMessage(Messages.PERMIT_TELEPORT)
+            target.sendMessage(Messages.SUCCESSFULLY_TELEPORT)
+            status.accepted = true
+        }else{
+            executor.sendMessage(Messages.NOT_FOUND_REQUEST)
+            executor.sendMessage(Messages.HINT_RESEND_REQUEST)
         }
     }
 }
